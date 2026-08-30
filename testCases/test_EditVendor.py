f5b6dbf3d7ef558102e33f28358dfddfb03a7d3d
VemulaHimasai <himasaiv9@gmail.com>
2026-08-30 09:57:38 +0530

    modified edit code

--- testCases/test_EditVendor.py
+++ testCases/test_EditVendor.py
@@ -7,55 +7,88 @@ from pageObjects.SearchVendorPage import SearchVendorPage
 from utilities.readproperties import ReadConfig
 from utilities.customLogger import LogGen
 
+
 class Test_EditVendor_014:
+
     baseURL = ReadConfig.getApplicationURL()
     username = ReadConfig.getUseremail()
     password = ReadConfig.getPassword()
     logger = LogGen.loggen()
 
-    @pytest.mark.regression
-    def test_edit_vendor(self,setup):
+    def test_edit_vendor(self, setup):
+
         self.logger.info("******* Test_EditVendor_014 *******")
+
         self.driver = setup
+
         self.driver.get(self.baseURL)
+
         self.driver.maximize_window()
+
         self.driver.implicitly_wait(10)
 
+        # Login
         self.lp = LoginPage(self.driver)
+
         self.lp.setUserName(self.username)
+
         self.lp.setPassword(self.password)
+
         self.lp.clickLogin()
 
-        self.logger.info("***********Login Successful**********")
+        self.logger.info("*********** Login Successful **********")
 
-        self.logger.info("**********Starting EditVendor_014**********")
-
+        # Navigate to Vendors
         self.addcust = AddCustomer(self.driver)
+
         self.addcust.clickOnCustomersMenu()
 
         self.addvendor = AddVendor(self.driver)
+
         self.addvendor.clickonVendorMenuItem()
 
-        self.logger.info("********edit vendor**********")
+        self.logger.info("********** Starting EditVendor_014 **********")
+
+        # Search Vendor
         searchvendor = SearchVendorPage(self.driver)
+
         old_vendor_name = "Vendor2"
+
         searchvendor.setName(old_vendor_name)
+
         searchvendor.clickSearch()
 
+        # Verify vendor exists
         status = searchvendor.searchVendorByName(old_vendor_name)
 
-        assert status is True, "Vendor edit failed"
+        assert status is True
 
-        self.logger.info(f"Vendor found: {old_vendor_name}")
-        self.logger.info("*****Clicked Edit Button******")
+        self.logger.info(
+            f"Vendor found: {old_vendor_name}"
+        )
 
-        searchvendor.clickEditVendorByName(old_vendor_name)
+        # IMPORTANT: Click Edit button
+        searchvendor.clickEditVendorByName(
+            old_vendor_name
+        )
 
+        self.logger.info(
+            "***** Clicked Edit Button *****"
+        )
+
+        # Edit vendor name
         new_vendor_name = "Vendor2_update"
-        self.addvendor.editVendorName(new_vendor_name)
 
+        self.addvendor.editVendorName(
+            new_vendor_name
+        )
+
+        # Save changes
         self.addvendor.clickSave()
 
-        self.logger.info(f"Vendor name updated from {old_vendor_name} to {new_vendor_name}")
+        self.logger.info(
+            f"Vendor name updated from "
+            f"{old_vendor_name} to {new_vendor_name}"
+        )
 
-        print("Updated Vendor Name: ", new_vendor_name)
+        self.driver.close()
