e5314d2c0cc408192b38d796efb8c01e6e53684d
VemulaHimasai <himasaiv9@gmail.com>
2026-09-25 17:53:41 +0530

    modified batch file

--- run.bat
+++ run.bat
@@ -14,9 +14,9 @@ echo Python version:
 echo.
 echo Running pytest...
 
-@REM .venv\Scripts\python.exe -m pytest -v -s -m "sanity or regression" --html=./Reports/report_chrome.html testCases/ --browser chrome
+.venv\Scripts\python.exe -m pytest -v -s -m "sanity or regression" --html=./Reports/report_chrome.html testCases/ --browser chrome
 
-.venv\Scripts\python.exe -m pytest -v -s -m "sanity or regression" --html=./Reports/report_firefox.html testCases/ --browser firefox
+@REM .venv\Scripts\python.exe -m pytest -v -s -m "sanity or regression" --html=./Reports/report_firefox.html testCases/ --browser firefox
 
 echo.
 echo ========================================
